c4a18e7299aa4731964fbdc9d8bf76a511f175fe
Kaio Silva <kaio.silva@rd.local>
2019-12-16 14:20:04 -0300

    created avatar component

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,6 @@
 import React from "React"
 import { useStaticQuery, graphql } from "gatsby"
+import Avatar from "../Avatar"
 
 const Profile = () => {
   const {
@@ -20,6 +21,7 @@ const Profile = () => {
 
   return (
     <div className="Profile-wrapper">
+      <Avatar />
       <h1>{title}</h1>
       <h2>{position}</h2>
       <p>{description}</p>
