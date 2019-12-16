9424975f15fef3615f4ab76e67d4c4db33652adf
Kaio Silva <kaio.silva@rd.local>
2019-12-16 00:47:09 -0300

    created new component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "React"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Kaio Felipe Silva</h1>
+    <h2>Tech Recruiter</h2>
+    <p>Membro do Core Team da Empresa Foo, escrevo sobre o mundo recruiter e outras coisas mais.</p>
+  </div>
+)
+
+export default Profile
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "React"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>About page</h1>
+  </Layout>
+)
+
+export default AboutPage
