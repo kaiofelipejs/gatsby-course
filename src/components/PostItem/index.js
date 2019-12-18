08a3111e5590b21bf3b56fd9d525c070f095d6c6
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2019-12-18 12:50:34 -0300

    Create props to PostItem

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,22 +1,28 @@
 import React from "react"
+import PropTypes from "prop-types"
 import * as S from "./styled"
 
-const PostItem = () => (
-  <S.PostItemLink to="/slug/">
+const PostItem = ({ slug, background, category, date, timeToRead, title, description }) => (
+  <S.PostItemLink to={slug}>
     <S.PostItemWrapper>
-      <S.PostItemTag background="#47650b">Misc</S.PostItemTag>
+      <S.PostItemTag background={background}>{category}</S.PostItemTag>
       <S.PostItemInfo>
-        <S.PostItemDate>18 de Dezembro de 2019 • 5 min de leitura</S.PostItemDate>
-        <S.PostItemTitle>
-          Diga não ao Medium: tenha sua própria plataforma
-        </S.PostItemTitle>
-        <S.PostItemDescription>
-          Algumas razões para você ter sua própria plataforma ao invés de
-          soluções como o Medium.
-        </S.PostItemDescription>
+        <S.PostItemDate>{date} • {timeToRead} min de leitura</S.PostItemDate>
+        <S.PostItemTitle>{title}</S.PostItemTitle>
+        <S.PostItemDescription>{description}</S.PostItemDescription>
       </S.PostItemInfo>
     </S.PostItemWrapper>
   </S.PostItemLink>
 )
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  background: PropTypes.string,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+}
 
 export default PostItem
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,22 +6,14 @@ import PostItem from "../components/PostItem"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
+    <PostItem
+      slug="/about/"
+      category="Misc"
+      date="18 de Dezembro de 2019"
+      timeToRead="5"
+      title="Diga não ao Medium: tenha sua própria plataforma"
+      description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
+    />
   </Layout>
 )
 
