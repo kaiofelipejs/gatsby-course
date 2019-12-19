26523e73dd9c52094d3690eca885ff1b4bb3eabd
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2019-12-19 13:19:38 -0300

    add comments with disqus

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,6 +5,7 @@ import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import * as S from "../components/Post/styled"
 import RecommendedPost from "../components/RecommendedPosts"
+import Comments from "../components/Comments"
 
 const BlogPost = ({ data, pageContext }) => {
   const post = data.markdownRemark
@@ -25,6 +26,7 @@ const BlogPost = ({ data, pageContext }) => {
         <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
       </S.MainContent>
       <RecommendedPost next={next} previous={previous} />
+      <Comments url={post.fields.slug} title={post.frontmatter.title} />
     </Layout>
   )
 }
@@ -32,6 +34,9 @@ const BlogPost = ({ data, pageContext }) => {
 export const query = graphql`
   query Post($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
+      fields {
+        slug
+      }
       frontmatter {
         title
         description
