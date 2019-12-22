7164bdb3a1cb59bfafba10520e7f1277f80910cf
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2019-12-22 01:33:16 -0300

    add grid styles

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -5,6 +5,7 @@ import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
 import Pagination from "../components/Pagination"
+import * as S from "../components/ListWrapper/styled"
 
 const BlogList = props => {
   const postList = props.data.allMarkdownRemark.edges
@@ -18,25 +19,27 @@ const BlogList = props => {
   return (
     <Layout>
       <SEO title="Home" />
-      {postList.map(
-        ({
-          node: {
-            frontmatter: { background, category, date, description, title },
-            timeToRead,
-            fields: { slug },
-          },
-        }) => (
-          <PostItem
-            slug={slug}
-            background={background}
-            category={category}
-            date={date}
-            timeToRead={timeToRead}
-            title={title}
-            description={description}
-          />
-        )
-      )}
+      <S.ListWrapper>
+        {postList.map(
+          ({
+            node: {
+              frontmatter: { background, category, date, description, title },
+              timeToRead,
+              fields: { slug },
+            },
+          }) => (
+            <PostItem
+              slug={slug}
+              background={background}
+              category={category}
+              date={date}
+              timeToRead={timeToRead}
+              title={title}
+              description={description}
+            />
+          )
+        )}
+      </S.ListWrapper>
     <Pagination 
       isFirst={isFirst}
       isLast={isLast}
