74faa36c0265cae1f9a9068e7818b9caefdf85c6
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2019-12-22 01:09:28 -0300

    create setTheme method

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import { Home } from "styled-icons/boxicons-solid/Home"
 import { SearchAlt2 as Search } from "styled-icons/boxicons-regular/SearchAlt2"
@@ -7,8 +7,18 @@ import { Bulb as Light } from "styled-icons/boxicons-regular/Bulb"
 import { Grid } from "styled-icons/boxicons-solid/Grid"
 import * as S from "./styled"
 
-const MenuBar = () => (
-  <S.MenuBarWrapper>
+const MenuBar = () => {
+  const [theme, setTheme] = useState(null)
+
+  const isDarkMode = theme === 'dark'
+
+  useEffect(() => {
+    setTheme(window.__theme)
+    window.__onThemeChange = () => setTheme(window.__theme)
+  }, [])
+
+  return (
+    <S.MenuBarWrapper>
     <S.MenuBarGroup>
       <S.MenuBarLink to="/" title="Voltar para home">
         <S.MenuBarItem><Home /></S.MenuBarItem>
@@ -20,11 +30,20 @@ const MenuBar = () => (
     </S.MenuBarGroup>
     
     <S.MenuBarGroup>
-      <S.MenuBarItem to="/" title="Mudar tema"><Light /></S.MenuBarItem>
+      <S.MenuBarItem to="/" 
+        title="Mudar tema" 
+        onClick={() => {
+          window.__setPreferredTheme(isDarkMode ? 'light' : 'dark')
+        }}
+        className={theme}
+      >
+        <Light />
+      </S.MenuBarItem>
       <S.MenuBarItem to="/" title="Mudar visualização"><Grid /></S.MenuBarItem>
       <S.MenuBarItem to="/" title="Ir para o topo"><Arrow /></S.MenuBarItem>
     </S.MenuBarGroup>
   </S.MenuBarWrapper>
-)
+  )
+}
 
 export default MenuBar
