f6a2a20916f6382ce0157b2c14d0bb6d27ac762f
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2019-12-22 01:19:17 -0300

    add display method

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -5,16 +5,23 @@ import { SearchAlt2 as Search } from "styled-icons/boxicons-regular/SearchAlt2"
 import { UpArrowAlt as Arrow } from "styled-icons/boxicons-regular/UpArrowAlt"
 import { Bulb as Light } from "styled-icons/boxicons-regular/Bulb"
 import { Grid } from "styled-icons/boxicons-solid/Grid"
+import { ThList as List } from "styled-icons/typicons/ThList"
+
 import * as S from "./styled"
 
 const MenuBar = () => {
   const [theme, setTheme] = useState(null)
+  const [display, setDisplay] = useState(null)
 
   const isDarkMode = theme === 'dark'
+  const isListMode = display === 'list'
 
   useEffect(() => {
     setTheme(window.__theme)
+    setDisplay(window.__display)
+
     window.__onThemeChange = () => setTheme(window.__theme)
+    window.__onDisplayChange = () => setDisplay(window.__display)
   }, [])
 
   return (
@@ -30,7 +37,7 @@ const MenuBar = () => {
     </S.MenuBarGroup>
     
     <S.MenuBarGroup>
-      <S.MenuBarItem to="/" 
+      <S.MenuBarItem
         title="Mudar tema" 
         onClick={() => {
           window.__setPreferredTheme(isDarkMode ? 'light' : 'dark')
@@ -39,7 +46,13 @@ const MenuBar = () => {
       >
         <Light />
       </S.MenuBarItem>
-      <S.MenuBarItem to="/" title="Mudar visualização"><Grid /></S.MenuBarItem>
+      <S.MenuBarItem 
+        title="Mudar visualização"
+        onClick={() => {
+          window.__setPreferredDisplay(isListMode ? 'grid' : 'list')
+      }}>
+        {isListMode ? <Grid /> : <List />}
+      </S.MenuBarItem>
       <S.MenuBarItem to="/" title="Ir para o topo"><Arrow /></S.MenuBarItem>
     </S.MenuBarGroup>
   </S.MenuBarWrapper>
