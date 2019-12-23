18b245d5b2f4a6ef68f976b0a05c6684520ba847
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2019-12-22 23:21:18 -0300

    fixed sidebar and menubar in transition

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,18 +1,24 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { TransitionPortal } from "gatsby-plugin-transition-link"
+
 import Sidebar from "../Sidebar"
 import MenuBar from "../MenuBar"
-import * as S from "./styled"
 
+import * as S from "./styled"
 import GlobalStyles from "../../styles/global"
 
 const Layout = ({ children }) => {
   return (
     <S.LayoutWrapper>
       <GlobalStyles />
-      <Sidebar />
+      <TransitionPortal level="top">
+        <Sidebar />
+      </TransitionPortal>
       <S.LayoutMain>{children}</S.LayoutMain>
-      <MenuBar />
+      <TransitionPortal level="top">
+        <MenuBar />
+      </TransitionPortal>
     </S.LayoutWrapper>
   )
 }
