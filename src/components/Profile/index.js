97e9341777ab564757ab6967db4a56bb6910938e
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2019-12-24 11:45:26 -0300

    fixed some details

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,8 +1,8 @@
-import React from "React"
+import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Avatar from "../Avatar"
 import getThemeColor from "../../utils/getThemeColor"
+import Avatar from "../Avatar"
 import * as S from "./styled"
 
 const Profile = () => {
@@ -25,13 +25,13 @@ const Profile = () => {
   return (
     <S.ProfileWrapper>
       <S.ProfileLink
-        cover 
-        direction="left" 
+        to="/"
+        cover
+        direction="left"
         bg={getThemeColor()}
         duration={0.6}
-        to="/"
       >
-        <Avatar />  
+        <Avatar />
         <S.ProfileAuthor>
           {title}
           <S.ProfilePosition>{position}</S.ProfilePosition>
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 
-import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, Hits, Stats } from 'react-instantsearch-dom';
 
 import Hit from "./Hit"
@@ -12,11 +11,13 @@ const algolia = {
   indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME
 }
 
-const searchClient = algoliasearch(algolia.appId, algolia.searchOnlyApiKey);
-
 const Search = () => (
   <S.SearchWrapper>
-    <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
+    <InstantSearch
+      appId={algolia.appId}
+      apiKey={algolia.searchOnlyApiKey}
+      indexName={algolia.indexName}
+    >
       <SearchBox autoFocus translations={{ placeholder: 'Pesquisar...' }} />
       <Stats translations={{
         stats(nbHits, timeSpentMs) {
