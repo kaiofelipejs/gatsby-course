dfba8729acb14ae26c5cb2f97ed3aa2e573af792
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2019-12-24 11:53:15 -0300

    fixed typos

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,7 +1,7 @@
-import React from "React"
+import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
-import Search from "../components/search"
+import Search from "../components/Search"
 
 
 const SearchPage = () => (
